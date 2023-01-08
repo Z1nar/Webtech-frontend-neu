9f1f3cc3e206f057fff433920e05519812f60582
zinar alkis <crycoin366@gmail.com>
2023-01-08 23:44:49 +0100

    Wt-frontend

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <navbar></navbar>
+  <transition name="view" appear enter-active-class="animated zoomInRight" leave-active-class="animated zoomOutLeft">
+    <router-view></router-view>
+  </transition>
+<footer>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0);">
+    © {{year}} Copyright: DeShop
+  </div>
+</footer>
 </template>
+
+<script>
+
+import Navbar from './components/Navbar'
+export default {
+  name: 'App',
+  components: { Navbar },
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +32,29 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 960px;
+  margin: 0 auto;
 }
-
-nav {
+#nav {
   padding: 30px;
 }
-
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+#nav a.router-link-exact-active {
+  color: white;
+  background: crimson;
+}
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import AddProductView from "@/views/AddProductView";
+import ProductsView from "@/views/ProductsView";
+import AboutView from "@/views/AboutView";
+import ProductDetail from "@/views/ProductDetail";
 const routes = [
   {
     path: '/',
@@ -13,12 +16,33 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    //component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component:AboutView
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: ProductsView
+  },
+  {
+    path: '/add-product',
+    name: 'add-product',
+    component: AddProductView
+  },
+  {
+    path: '/demo',
+    name: 'demo-view',
+    component: () => import(/* webpackChunkName: "demo" */ '../views/Demo.vue')
+  },
+    {
+        path: '/products/:id',
+        name: 'product-detail',
+        component: ProductDetail
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
